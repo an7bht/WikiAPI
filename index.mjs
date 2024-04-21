b55d71ebff91f4850eaa2eefcf46ae703e77bad1
Đang Trương An <an7bht6@gmail.com>
2024-04-21 21:42:53 +0700

    fgfg

--- index.mjs
+++ index.mjs
@@ -1,14 +1,8 @@
 import express from 'express';
-import fs from 'fs';
 import axios from 'axios';
 
 const app = express();
 const port = process.env.PORT || 3000;
-// Middleware cho phép CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
 
 // Thông tin xác thực
 const username = 'wikia';
@@ -18,32 +12,39 @@ const headers = {
     'Authorization': `Basic ${auth}`
 };
 
-// Tạo một tuyến đường mới để lấy tất cả các URL từ WordPress REST API
+// Tạo tuyến đường GET "/getAllUrl"
 app.get('/getAllUrl', async (req, res) => {
     try {
-        const urls = await getAllURLs();
-        res.json({ status: "ok", urls });
+        let allUrls = [];
+
+        // Bắt đầu từ trang đầu tiên
+        let page = 1;
+
+        while (true) {
+            // Gửi yêu cầu để lấy dữ liệu từ trang hiện tại
+            const response = await axios.get(`https://wiki.retik.live/wp-json/wp/v2/posts?page=${page}`, { headers });
+            const posts = response.data;
+
+            // Trích xuất URL từ các bài viết và thêm vào mảng allUrls
+            posts.forEach(post => {
+                allUrls.push(post.link);
+            });
+
+            // Nếu không còn trang nào nữa, dừng vòng lặp
+            if (posts.length === 0) {
+                break;
+            }
+
+            // Tăng số trang lên 1 để lấy trang tiếp theo
+            page++;
+        }
+
+        res.json({ status: 'success', urls: allUrls });
     } catch (error) {
-        res.status(500).json({ status: "error", error: error.message });
+        console.error('Error:', error);
+        res.status(500).json({ status: 'error', message: 'Internal server error' });
     }
 });
-
-// Hàm lấy tất cả các URL từ WordPress REST API
-async function getAllURLs() {
-    const totalPosts = await axios.get('https://wiki.retik.live/wp-json/wp/v2/posts', { headers });
-    const totalPages = Math.ceil(totalPosts.headers['x-wp-total'] / totalPosts.headers['x-wp-totalpages']);
-
-    let allURLs = [];
-
-    for (let page = 1; page <= totalPages; page++) {
-        const response = await axios.get(`https://wiki.retik.live/wp-json/wp/v2/posts?page=${page}`, { headers });
-        const posts = response.data;
-        const urls = posts.map(post => post.link);
-        allURLs = allURLs.concat(urls);
-    }
-
-    return allURLs;
-}
 
 // Lắng nghe cổng và bắt đầu server
 app.listen(port, () => {
